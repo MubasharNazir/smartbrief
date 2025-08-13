4d3707e16c41b94ee2e440b29d774842515f0158
MubasharNazir <engr.mubasharnazir@gmail.com>
2025-08-13 09:43:07 +0400

    Initial commit for new repo

---
+++ src/firebase.js
@@ -0,0 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCU428L_pbQGOf2TUojhCzKXAEMzjAd_To",
+//   authDomain: "newsiq-6d817.firebaseapp.com",
+//   projectId: "newsiq-6d817",
+//   storageBucket: "newsiq-6d817.firebasestorage.app",
+//   messagingSenderId: "1071904845608",
+//   appId: "1:1071904845608:web:9300ab566d3316323aaa1d",
+//   measurementId: "G-VCPZNJDFSQ"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; // <-- Add this line
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCU428L_pbQGOf2TUojhCzKXAEMzjAd_To",
+  authDomain: "newsiq-6d817.firebaseapp.com",
+  projectId: "newsiq-6d817",
+  storageBucket: "newsiq-6d817.firebasestorage.app",
+  messagingSenderId: "1071904845608",
+  appId: "1:1071904845608:web:9300ab566d3316323aaa1d",
+  measurementId: "G-VCPZNJDFSQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app); // <-- Add this line
+
+export { db }; // <-- Add this line
